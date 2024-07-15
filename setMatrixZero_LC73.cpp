6fe3aca5bde424a43b5d0dff07633caa9282bb95
Alok-2004 <alokprasad02004@gmail.com>
2024-07-16 00:13:44 +0530

    200th Question

--- setMatrixZero_LC73.cpp
+++ setMatrixZero_LC73.cpp
@@ -1,3 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 200th leetcode Question
+
 
 // Time complexity is O(mn)
 // Space complexity is O(mn)
@@ -31,64 +36,64 @@ public:
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& m) {
-    	bool frstRowZero = false;
-    	bool frstclmnZero = false;
-    	
-    	for(int i = 0; i < m[0].size(); i++){
+        bool frstRowZero = false;
+        bool frstclmnZero = false;
+
+        for(int i = 0; i < m[0].size(); i++){
             if(m[0][i] == 0){
                 frstRowZero = true;
                 break;
             }
         }
 
-		for(int i = 0; i < m.size(); i++){
+        for(int i = 0; i < m.size(); i++){
             if(m[i][0] == 0){
                 frstclmnZero = true;
                 break;
             }
         }
 		
-		// creating Markers
-		for(int i = 1; i < m.size(); i++){
-			for(int j = 1; j<m[0].size(); j++){
-				if(m[i][j] == 0){
-					m[0][j] = 0;
-					m[i][0] = 0;
-				}
-			}
-		}
+        // creating Markers
+        for(int i = 1; i < m.size(); i++){
+            for(int j = 1; j < m[0].size(); j++){
+                if(m[i][j] == 0){
+                    m[i][0] = 0;
+                    m[0][j] = 0;
+                }
+            }
+        }
 		
-		// Modifing
-		for(int i = 1; i < m.size(); i++){
-			if(m[i][0] == 0){
-				for(int j = 1; j < m[0].size(); j++){
-					m[j][i] = 0;
-				}
-			}
-		}
+        // Modifying
+        for(int i = 1; i < m.size(); i++){
+            if(m[i][0] == 0){
+                for(int j = 1; j < m[0].size(); j++){
+                    m[i][j] = 0;
+                }
+            }
+        }
 		
-		for(int j = 1; i < m[0].size(); j++){
-			if(m[j][o] == 0){
-				for(int i = 1; i < m.size(); i++){
-					m[i][j] = 0;
-				}
-			}
-		}
+        for(int j = 1; j < m[0].size(); j++){
+            if(m[0][j] == 0){
+                for(int i = 1; i < m.size(); i++){
+                    m[i][j] = 0;
+                }
+            }
+        }
 		
-		// Now for 1st row and 1st column
-		if(frstRowZero){
-			for(int i = 0; i < m[0].size(); i++){
-				m[0][i] = 0;
-			}
-		}
-		if(frstclmnZero){
-			for(int i = 0; i < m.size(); i++){
-				m[i][0] = 0;
-			}
-		}
-		
-	}
+        // Now for 1st row and 1st column
+        if(frstRowZero){
+            for(int i = 0; i < m[0].size(); i++){
+                m[0][i] = 0;
+            }
+        }
+        if(frstclmnZero){
+            for(int i = 0; i < m.size(); i++){
+                m[i][0] = 0;
+            }
+        }
+    }
 };
+
 
 
 
